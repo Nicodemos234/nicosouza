291189254ff7321fedfa8ce7adbd2971e33700d0
Nicodemos234 <nicodemos54324@gmail.com>
2020-07-11 13:57:36 -0300

    Main Layout

---
+++ layouts/default.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <b-container>
+      <div class="ni-main-menu">
+        <img class="ni-logo" src="/images/ns.png" alt="NicoSouza logo">
+         <div>
+           <b-nav>
+             <b-nav-item>
+               <nuxt-link to="/">Início</nuxt-link>
+             </b-nav-item>
+              <b-nav-item>
+               <nuxt-link to="/trabalhos">Trabalhos</nuxt-link>
+             </b-nav-item>
+              <b-nav-item>
+               <nuxt-link to="/contato">Contato</nuxt-link>
+             </b-nav-item>
+           </b-nav>
+         </div>
+      </div>
+      <Nuxt />
+    </b-container>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+@import '../assets/styles/reset.css';
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+body {
+  background-color: #282A36;
+  font-family: 'Roboto', sans-serif;
+  color: #BCC2CD;
+}
+a {
+  color: #BCC2CD;
+  font-size: 30px;
+}
+a:hover {
+  color: #BD93F9;
+  text-decoration: none;
+}
+.ni-main-menu {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
+.ni-logo {
+  max-width: 115px;
+}
+
+h1 {
+  font-size: 64px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="ni-home">
+    <div class="ni-home-flex">
+      <div>
+        <h1>Seja bem-vindo</h1>
+        <div class="ni-home-text">
+          <p>Olá, meu nome é Nicodemos, tenho 18 anos,
+         sou programador, editor de vídeos e
+         apaixonado por tecnologia.</p>
+        </div>
+      </div>
+      <img src="/images/nico.png" alt="Nico" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.ni-home {
+  padding: 40px;
+}
+.ni-home-flex {
+  display: flex;
+  justify-content: space-between;
+}
+.ni-home-text {
+  font-size: 32px;
+  padding-top: 20px;
+  line-height: 36px;
+}
+</style>
