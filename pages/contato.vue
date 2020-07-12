687d20636b911d10d6ea3091a5ddb67842e77c3f
Nicodemos234 <nicodemos54324@gmail.com>
2020-07-12 16:28:23 -0300

    Bulding

--- pages/contato.vue
+++ pages/contato.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
-        
+    <div class="ni-home-flex">
+        <div>
+            <h1>Contato</h1>
+            <div>Você pode falar comigo pelo meu email: nicodemos54324@gmail.com</div>
+        </div>
     </div>
 </template>
